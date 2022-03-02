fcc15d1f903be36e0a2befa37776aab4b080f509
Eduardo Cabrera <eduardo.cabrera@rocket.chat>
2022-03-02 20:18:39 -0300

    chap7

---
+++ chap6/machineState/automaton_example_bits.asm
@@ -0,0 +1,38 @@
+section .text
+
+_A:
+    call getsymbol
+    cmp al, '+'
+    je _B
+    cmp al, '-'
+    je _B
+
+    cmp al '0'
+    jb _E 
+    cmp al, '9'
+    ja _E 
+    jmp _C
+
+_B:
+    call getsymbol
+    cmp al, '0'
+    jb _E
+    cmp al, '9'
+    ja _E
+    jmp _C
+
+_C:
+    call getsymbol
+    cmp al, '0'
+    jb _E
+    cmp al, '9'
+    ja _E  
+    test al, al
+    jz _D 
+    jmp _C 
+
+_D:
+    ; success
+
+_E:
+    ; error
